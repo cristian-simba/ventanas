7a70a2c0fef12617caef1285de705757f5d46572
cristian-simba <davidsimba200@gmail.com>
2023-06-20 09:46:55 -0500

    XD

--- src/PanelDibujo.java
+++ src/PanelDibujo.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class PanelDibujo {
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+public class PanelDibujo extends JPanel{
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        int ancho = getWidth();
+        int alto = getHeight();
+        /*g.drawLine(20,20,250,20);
+        g.drawLine(20,300,20,20);
+        g.drawLine(20,300,250,300);*/
+
+        Scanner ingreso = new Scanner(System.in);
+        /*String nombre;
+        System.out.print("Ingrese el nombre: ");
+        nombre = ingreso.nextLine();
+
+        g.drawString(nombre,50,50);*/
+
+        float estatura;
+        float peso;
+        float imc;
+
+        System.out.println("Ingrese su estatura: ");
+        estatura = ingreso.nextFloat();
+        System.out.println("Ingrese su peso: ");
+        peso = ingreso.nextFloat();
+
+        imc = peso / (estatura*estatura);
+
+    }
 }
--- src/PruebaPanelDibujo.java
+++ src/PruebaPanelDibujo.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class PruebaPanelDibujo {
+import javax.swing.*;
+
+public class PruebaPanelDibujo {
+    public static void main(String[] args) {
+        // Crear instancia de la clase PanelDibujo y se la llama panel
+        PanelDibujo panel = new PanelDibujo();
+        // Pa crear un marco en la ventana
+        JFrame aplicacion = new JFrame();
+        // Poner un botón pa cerrar la ventana
+        aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        aplicacion.add(panel); // Agregar el panel a la ventana
+        aplicacion.setSize(420,400); // Definir el tamaño de la ventana
+        aplicacion.setVisible(true); // Pa que se pueda ver la ventana
+    }
 }
